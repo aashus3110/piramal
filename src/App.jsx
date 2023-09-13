123731e93e671a77dfad3cbcf158966f1fa05dfe
ashu3110 <avsingh000.789@gmail.com>
2023-09-13 17:40:42 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/piramal" element={<Register />} />
         <Route path="/SearchPage" element={<SearchPage />} />
       </Routes>
     </Router>
